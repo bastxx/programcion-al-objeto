43ace35c50fae50cbda47b4c8d649c049faa3334
bastxx <bastian.araya30@inacapmail.cl>
2022-10-05 20:57:51 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class animal:
+    def __init__(self) -> None:
+        pass
+class gato:
+        def __init__(self) -> None:
+            pass
+class perro:
+    def __init__(self) -> None:
+        pass
+class vaca:
+    def __init__(self) -> None:
+        pass
+        
